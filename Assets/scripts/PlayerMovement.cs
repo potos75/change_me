e9bb515966601d52777263e2785d328ec7b373a8
SpoconaStopa <kacperjanas2008@gmail.com>
2024-10-03 22:33:09 +0200

    Player movment
    basic movment of player

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -6,48 +6,33 @@ using UnityEngine.UIElements;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rigB;
-    public int PlayerSpeed = 2;
+    public int PlayerSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        PlayerSpeed /= 2;
-        Debug.Log(rigB.velocity.y);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        rigB.velocity = Vector2.up * 0;
         if (Input.GetKey(KeyCode.W) == true)
         {
-            rigB.velocity =  Vector2.up * PlayerSpeed;
-        }
-        else
-        {
-            rigB.velocity += Vector2.up * 0;
+            rigB.velocity += Vector2.up * PlayerSpeed;
         }
         if (Input.GetKey(KeyCode.S) == true)
         {
             rigB.velocity += Vector2.down * PlayerSpeed;
         }
-        else
-        {
-            rigB.velocity += Vector2.up * 0;
-        }
         if (Input.GetKey(KeyCode.A) == true)
         {
             rigB.velocity += Vector2.left * PlayerSpeed;
-        }
-        else
-        {
-            rigB.velocity += Vector2.left * 0;
         }
         if (Input.GetKey(KeyCode.D) == true)
         {
             rigB.velocity += Vector2.right * PlayerSpeed;
         }
-        else
-        {
-            rigB.velocity += Vector2.right * 0;
-        }
+
     }
 }
