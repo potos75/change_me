cf6fdbe7f9e0778d2bb3372e6322d4a0b19ac7e9
SpoconaStopa <kacperjanas2008@gmail.com>
2024-10-03 22:45:00 +0200

    player movment repair

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -6,49 +6,33 @@ using UnityEngine.UIElements;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rigB;
-    public int PlayerSpeed = 2;
+    public int PlayerSpeed;
     public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        PlayerSpeed /= 2;
-        Debug.Log(rigB.velocity.y);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        rigB.velocity = Vector2.up * 0;
         if (Input.GetKey(KeyCode.W) == true)
         {
             rigB.velocity =  Vector2.up * PlayerSpeed;
-        }
-        else
-        {
-            rigB.velocity += Vector2.up * 0;
         }
         if (Input.GetKey(KeyCode.S) == true)
         {
             rigB.velocity += Vector2.down * PlayerSpeed;
         }
-        else
-        {
-            rigB.velocity += Vector2.up * 0;
-        }
         if (Input.GetKey(KeyCode.A) == true)
         {
             rigB.velocity += Vector2.left * PlayerSpeed;
         }
-        else
-        {
-            rigB.velocity += Vector2.left * 0;
-        }
         if (Input.GetKey(KeyCode.D) == true)
         {
             rigB.velocity += Vector2.right * PlayerSpeed;
-        }
-        else
-        {
-            rigB.velocity += Vector2.right * 0;
         }
     }
 }
