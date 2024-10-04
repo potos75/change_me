b045066aa9621c32f8982e666400dcf9cb9a1eb0
SpoconaStopa <kacperjanas2008@gmail.com>
2024-10-04 23:24:35 +0200

    start of making inventory v1
    i started making an inventory and add a lot of mess to the code but i will clean it later

---
+++ Assets/scripts/logiscript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logiscript : MonoBehaviour
+{
+    /*
+     by³ to jakiœ pomys³ ale zrezygnowa³em z niego na ¿ecz guzika ale mo¿e mi siê jeszcze przyda
+    public GameObject mainmenu;
+    public bool ismainmenuon;
+
+    void Update()
+    {
+        if (ismainmenuon == true)
+        {
+            if (Input.GetKey(KeyCode.Q) == true)
+            {
+                mainmenu.SetActive(false);
+                ismainmenuon = false;
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.Q) == true)
+            {
+                mainmenu.SetActive(true);
+                ismainmenuon = true;
+            }
+        }
+    }
+    */
+
+}
+
